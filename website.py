0e79c8e8bc379944c955bd710f2ea848065c1402
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-03-14 19:03:08 +0530

    Fix indentation and improve file handling logic

--- website.py
+++ website.py
@@ -39,10 +39,9 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
     if uploaded_file is not None:
         if uploaded_file.name.endswith(".xlsx"):
         data = pd.read_excel(uploaded_file, header=None)
-
-         else: 
+        else: 
         data = pd.read_csv(uploaded_file, sep=None, engine="python", header=None)
-
+        st.write(data)
         omega_exp = data.iloc[:,0].values
         I_exp = data.iloc[:,1].values
 
