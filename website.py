af0b783f090fe68f31403b812f7e390ee1daec79
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-03-14 19:07:59 +0530

    Fix file upload message and improve code formatting

--- website.py
+++ website.py
@@ -5,11 +5,11 @@ import pandas as pd
 from scipy.integrate import simpson
 from scipy.optimize import curve_fit
 
-
 st.set_page_config(page_title="Raman-Fano line-shape", layout="centered")
 
 st.title("Raman-Fano line-shape plot")
 st.write("Enter parameters and upload file")
+
 # -------- PARAMETER ----------
 st.subheader("Enter Parameters")
 
@@ -28,98 +28,86 @@ with col3:
 st.subheader("Upload Raman File")
 
 uploaded_file = st.file_uploader(
-    "Upload CSV/TXT file (two columns)",
-    type=["csv","txt","xlsx"]
+    "Upload CSV/TXT/XLSX file (two columns)",
+    type=["csv", "txt", "xlsx"]
 )
-
 
 # -------- LOAD DATA ----------
 with st.spinner("Fitting Raman spectrum... Please wait"):
 
- if uploaded_file is not None:
-        if uploaded_file.name.endswith(".xlsx"):
-           data = pd.read_excel(uploaded_file, header=None)
-        else: 
-           data = pd.read_csv(uploaded_file, sep=None, engine="python", header=None)
-        st.write(data)
-     
-        omega_exp = data.iloc[:,0].values
-        I_exp = data.iloc[:,1].values
+    if uploaded_file is not None:
 
-         # fitting range
+        if uploaded_file.name.endswith(".xlsx"):
+            data = pd.read_excel(uploaded_file, header=None)
+        else:
+            data = pd.read_csv(uploaded_file, sep=None, engine="python", header=None)
+
+        st.write(data)
+
+        omega_exp = data.iloc[:, 0].values
+        I_exp = data.iloc[:, 1].values
+
+        # fitting range
         mask = (omega_exp >= 440) & (omega_exp <= 560)
         omega_exp = omega_exp[mask]
         I_exp = I_exp[mask]
 
-        # -------- SORT DATA (improves fitting stability) ----------
+        # -------- SORT DATA ----------
         idx = np.argsort(omega_exp)
         omega_exp = omega_exp[idx]
         I_exp = I_exp[idx]
 
-
         omega_peak = omega_exp[np.argmax(I_exp)]
-        #st.write("Experimental peak =", omega_peak)
-        # k values (high resolution better)
+
         # k grid
-        k = np.linspace(0,1,2000)
+        k = np.linspace(0, 1, 2000)
 
-     #q=3
-     # -------- omega(k) ----------
-
-        
+        # -------- omega(k) ----------
         omega_k_vals = np.sqrt(A + B*np.cos(np.pi*k/2))
 
-       # -------- integration ----------
+        # -------- FANO MODEL ----------
+        def fano_model(omega, q, L, Gamma, shift, C, m, c):
 
+            omega2D = omega[:, None] + shift
 
+            eps = (omega2D - omega_k_vals) / (Gamma/2)
 
+            integrand = np.exp(-(k**2 * L**2)/(4*a**2)) * ((q+eps)**2/(1+eps**2))
 
-        def fano_model(omega,q,L,Gamma,shift,C,m,c):
+            integrand *= (2*np.pi*k)
 
-         omega2D = omega[:,None] + shift
+            I = np.trapezoid(integrand, k, axis=1)
 
-         eps = (omega2D - omega_k_vals)/(Gamma/2)
+            background = m*omega + c
 
-         integrand = np.exp(-(k**2 * L**2)/(4*a**2)) * ((q+eps)**2/(1+eps**2))
+            return C*I + background
 
-         integrand *= (2*np.pi*k)
+        popt, _ = curve_fit(
+            fano_model,
+            omega_exp,
+            I_exp,
+            p0=[2,5,6,0,100,0,10],
+            bounds=([-50,0,1,-10,0,-10,-500],
+                    [50,50,30,10,1e6,10,500]),
+            maxfev=40000
+        )
 
-         I = np.trapezoid(integrand, k, axis=1)
+        q, L, Gamma, shift, C, m, c = popt
 
-         background = m*omega + c
-
-         return C*I + background
-
-        popt,_ = curve_fit(
-             fano_model,
-             omega_exp,
-             I_exp,
-             p0=[2,5,6,0,100,0,10],
-             bounds=([-50,0,1,-10,0,-10,-500],
-             [50,50,30,10,1e6,10,500]),
-             maxfev=40000
-           )
-
-    
-        q,L,Gamma,shift,C,m,c = popt
-
-      # fitted curve
         fit = fano_model(omega_exp, *popt)
 
         st.subheader("Final Fitted Values")
-        st.write("q =",round(q,3))
-        st.write("L =",round(L,3),"nm")
-        st.write("Gamma =",round(Gamma,3))
-        #print("Shift =",round(shift,3))
-        #st.write("omega_k_val =",omega_k_vals)
+        st.write("q =", round(q,3))
+        st.write("L =", round(L,3), "nm")
+        st.write("Gamma =", round(Gamma,3))
 
-
-# plot
+        # plot
         fig, ax = plt.subplots(figsize=(6,5))
-        ax.plot(omega_exp,I_exp,'r.',label="Experimental")
-        ax.plot(omega_exp,fano_model(omega_exp,*popt),'b-',label="Fitted")
+        ax.plot(omega_exp, I_exp, 'r.', label="Experimental")
+        ax.plot(omega_exp, fit, 'b-', label="Fitted")
         ax.legend()
         ax.grid()
+
         st.pyplot(fig)
 
     else:
