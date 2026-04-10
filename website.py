103e77aba858a626bfd74ce53cfc736896f019b1
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-04-10 15:55:19 +0530

    Change initial parameters for curve fitting

--- website.py
+++ website.py
@@ -89,7 +89,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 fano_model,
                 omega_exp,
                 I_exp,
-                p0=[10, 5, 6, 0, 100, 0, 10],
+                p0=[5, 5, 6, 0, 100, 0, 10],
                 bounds=([-50, 0, 1, -10, 0, -10, -500],
                         [50, 50, 30, 10, 1e6, 10, 500]),
                 maxfev=40000
@@ -125,7 +125,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 model_fixed_L,
                 omega_exp,
                 I_exp,
-                p0=[10, 6, 0, 100, 0, 10],
+                p0=[5, 6, 0, 100, 0, 10],
                 bounds=([-50, 1, -10, 0, -10, -500],
                         [50, 30, 10, 1e6, 10, 500]),
                 maxfev=40000
