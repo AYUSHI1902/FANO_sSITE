743858f656cc6b1afee96d3b9afcbf67ac2289c6
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-04-12 21:04:37 +0530

    Increase L value from 500 to 1000

--- website.py
+++ website.py
@@ -133,7 +133,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
             )
 
             q, Gamma, shift, C, m, c = popt
-            L = 500
+            L = 1000
 
         # -------- FINAL FIT ----------
         fit = fano_model(omega_exp, q, L, Gamma, shift, C, m, c)
