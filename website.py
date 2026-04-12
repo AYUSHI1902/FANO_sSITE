54421d8ff13f8cb1bda787ef2e31b67b15e10ad0
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-04-12 21:05:34 +0530

    Update initial parameters for curve fitting

--- website.py
+++ website.py
@@ -126,7 +126,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 model_fixed_L,
                 omega_exp,
                 I_exp,
-                p0=[0, 6, 0, 100, 0, 10],
+                p0=[10, 6, 0, 100, 0, 10],
                 bounds=([-50, 1, -10, 0, -10, -500],
                         [50, 30, 10, 1e6, 10, 500]),
                 maxfev=40000 
