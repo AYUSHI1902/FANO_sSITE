838f1498f219a4d78656a37e16e4595041667a7b
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-04-13 13:36:57 +0530

    Fix initial parameters for curve fitting

--- website.py
+++ website.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from scipy.optimize import curve_fit
 from scipy.interpolate import interp1d
+
 st.set_page_config(page_title="Raman-Fano line-shape", layout="centered")
 
 st.title("Raman-Fano line-shape plot")
@@ -11,7 +12,6 @@ st.write("Enter parameters and upload file")
 
 # -------- PARAMETERS ----------
 st.subheader("Enter Parameters")
-
 
 col1, col2, col3 = st.columns(3)
 
@@ -82,7 +82,9 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
             background = m * omega + c
 
             return C * I + background
+
         #infodict = {"nfev": None}
+
         # -------- FITTING ----------
         if mode == "Fano and Confinement":
 
@@ -90,7 +92,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 fano_model,
                 omega_exp,
                 I_exp,
-                p0=[1, 5, 6, 0, 100, 0, 10],
+                p0=[0, 5, 6, 0, 100, 0, 10],
                 bounds=([-50, 0, 1, -10, 0, -10, -500],
                         [50, 50, 30, 10, 1e6, 10, 500]),
                 maxfev=40000
@@ -107,15 +109,14 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 model_fixed_q,
                 omega_exp,
                 I_exp,
-                p0=[5, 6, 0, 100, 0, 10],
-                bounds=([-50, 1, -10, 0, -10, -500],
+                p0=[05, 6, 0, 100, 0, 10],
+                bounds=([0, 1, -10, 0, -10, -500],
                         [50, 30, 10, 1e6, 10, 500]),
-                maxfev=40000 
+                maxfev=40000
             )
 
             L, Gamma, shift, C, m, c = popt
-            
-            q = 100
+            q = 1000
 
         elif mode == "Fano":
 
@@ -126,10 +127,10 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 model_fixed_L,
                 omega_exp,
                 I_exp,
-                p0=[-50, 6, 0, 100, 0, 10],
+                p0=[1, 6, 0, 100, 0, 10],
                 bounds=([-50, 1, -10, 0, -10, -500],
                         [50, 30, 10, 1e6, 10, 500]),
-                maxfev=40000 
+                maxfev=40000
             )
 
             q, Gamma, shift, C, m, c = popt
@@ -137,12 +138,9 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
 
         # -------- FINAL FIT ----------
         fit = fano_model(omega_exp, q, L, Gamma, shift, C, m, c)
+
         #interp_func = interp1d(omega_exp, I_exp, kind='cubic')
-
         #fit = interp_func(omega_exp)
-
-       
-     
 
         # -------- OUTPUT ----------
         st.subheader("Final Fitted Values")
@@ -150,11 +148,12 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
         st.write("q =", round(q, 3))
         st.write("L =", round(L, 3), "nm")
         st.write("Gamma =", round(Gamma, 3))
-       
+
         # -------- PLOT ----------
         fig, ax = plt.subplots(figsize=(6, 5))
         ax.plot(omega_exp, I_exp, 'r.', label="Experimental")
         ax.plot(omega_exp, fit, 'b-', label="Fitted")
+
         ax.legend()
         ax.grid()
 
