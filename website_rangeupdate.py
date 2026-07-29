57a21e2df7ac49d5ee12a08c7f217df6b039fa0e
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-07-29 15:21:57 +0530

    Increase maxfev parameter and adjust model parameters

--- website_rangeupdate.py
+++ website_rangeupdate.py
@@ -113,7 +113,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 p0=[0, 5, 6, 0, 100, 0, 10],
                 bounds=([-10, 0, 1, -10, 0, -10, -500],
                         [10, 50, 30, 10, 1e6, 10, 500]),
-                maxfev=4000,
+                maxfev=40000,
                 full_output=True 
             )
 
@@ -131,7 +131,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 p0=[1, 6, 0, 100, 0, 10],
                 bounds=([0, 1, -10, 0, -10, -500],
                         [50, 30, 10, 1e6, 10, 500]),
-                maxfev=4000
+                maxfev=40000
             )
 
             L, Gamma, shift, C, m, c = popt
@@ -141,7 +141,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
         elif mode == "Fano":
 
             def model_fixed_L(omega, q, Gamma, shift, C, m, c):
-                return fano_model(omega, q, 1000, Gamma, shift, C, m, c)
+                return fano_model(omega, q, 100, Gamma, shift, C, m, c)
 
             popt, _ = curve_fit(
                 model_fixed_L,
@@ -150,11 +150,11 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
                 p0=[0, 6, 0, 100, 0, 10],
                 bounds=([-10, 1, -10, 0, -10, -500],
                         [10, 30, 10, 1e6, 10, 500]),
-                maxfev=4000
+                maxfev=40000
             )
 
             q, Gamma, shift, C, m, c = popt
-            L = 1000
+            L = 100
             #st.write("sample is having Fano effect")
         # -------- FINAL FIT ----------
         fit = fano_model(omega_exp, q, L, Gamma, shift, C, m, c)
