2308b4d1d0123b99720119a9515943677f9d5186
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-07-29 19:03:02 +0530

    Change fixed L value from 50 to 100 in Fano model

--- website_rangeupdate.py
+++ website_rangeupdate.py
@@ -141,7 +141,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
         elif mode == "Fano":
 
             def model_fixed_L(omega, q, Gamma, shift, C, m, c):
-                return fano_model(omega, q, 50, Gamma, shift, C, m, c)
+                return fano_model(omega, q, 100, Gamma, shift, C, m, c)
 
             popt, _ = curve_fit(
                 model_fixed_L,
@@ -154,7 +154,7 @@ with st.spinner("Fitting Raman spectrum... Please wait"):
             )
 
             q, Gamma, shift, C, m, c = popt
-            L = 50
+            L = 100
             #st.write("sample is having Fano effect")
         # -------- FINAL FIT ----------
         fit = fano_model(omega_exp, q, L, Gamma, shift, C, m, c)
