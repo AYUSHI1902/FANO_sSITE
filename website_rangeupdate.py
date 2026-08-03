abc35001ebdb16291c808443790a5e533bec5437
AYUSHI1902 <57395152+AYUSHI1902@users.noreply.github.com>
2026-08-03 13:40:36 +0530

    Update website_rangeupdate.py

--- website_rangeupdate.py
+++ website_rangeupdate.py
@@ -11,7 +11,7 @@ from reportlab.lib.pagesizes import letter
 
 st.set_page_config(page_title="Raman-Fano line-shape", layout="centered")
 
-st.title("Raman-Fano line-shape plot")
+st.title("Raman-Fano, line-shape plot")
 st.write("Enter parameters and upload file")
 
 # -------- PARAMETERS ----------
